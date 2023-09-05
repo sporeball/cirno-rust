84688acebb73eb142bead8bf298d02ec4ce0992f
sporeball <sporeballdev@gmail.com>
2023-09-04 22:06:19 -0700

    file read

--- cirno/src/main.rs
+++ cirno/src/main.rs
@@ -1,5 +1,22 @@
-use std::io;
+use std::io::{self, prelude::*, BufReader};
+use std::fs::File;
+use clap::Parser;
 
-fn main() {
-  println!("hello cirno");
+/// Full-featured circuit design tool
+#[derive(Parser)]
+struct Cli {
+  filename: std::path::PathBuf,
+}
+
+fn main() -> Result<(), io::Error> {
+  let args = Cli::parse();
+  let filename = args.filename.to_str().unwrap();
+  let file = File::open(filename)?;
+  let contents = BufReader::new(file);
+
+  for line in contents.lines() {
+    println!("{}", line.unwrap());
+  }
+
+  Ok(())
 }
